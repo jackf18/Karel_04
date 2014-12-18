df2524927c5405156bee062668cd371f0546f0c3
jackf18 <jackf18@hsln_17581.local>
2014-12-18 14:22:00 +0000

    I did the assignment

--- Lab04.java
+++ Lab04.java
@@ -2,16 +2,71 @@
 *
 * Description of the program goes here  // provide a brief description
 *
-* @author <Your Name here>  // replace <...> with your name
-* @version <date/of/completion> // replace <...> with the date
+* @author <Jack Flaherty>  // replace <...> with your name
+* @version <3/11/14> // replace <...> with the date
 */
 
 import edu.fcps.karel2.Display;
 import edu.fcps.karel2.Robot;
 
 public class Lab04 {
-
+   
+     public static void takeTheField(Athlete arg)
+     {
+         arg.move();
+         arg.move();
+         arg.move();
+         arg.move();
+         arg.turnRight();
+         arg.move();
+         arg.move();
+     }
      public static void main(String[] args) {
-          // TODO Your code goes here
+     Display.openWorld("maps/arena.map");
+     Display.setSize(10, 10);
+     Display.setSpeed(8);
+     Athlete jack = new Athlete();
+     Athlete michael = new Athlete();
+     Athlete tommy = new Athlete();
+     Athlete mark = new Athlete();
+     Athlete maia = new Athlete();
+     Athlete kerry = new Athlete (2, 7, Display.EAST, 0); // you could also position all the players using this kind of constructor.
+     takeTheField(jack);
+     takeTheField(michael);
+     takeTheField(tommy);
+     takeTheField(mark);
+     takeTheField(maia);
+     takeTheField(kerry);
+     jack.move();
+     jack.move();
+     jack.move();
+     jack.turnRight();
+     michael.move();
+     michael.move();
+     michael.move();
+     michael.turnLeft();
+     michael.move();
+     michael.move();
+     michael.turnAround();
+     tommy.move();
+     tommy.turnLeft();
+     tommy.move();
+     tommy.turnAround();
+     maia.move();
+     maia.move();
+     maia.turnRight();
+     kerry.move();
+     kerry.move();
+     kerry.move();
+     kerry.move();
+     kerry.turnRight();
+     mark.move();
+     mark.move();
+     mark.move();
+     mark.move();
+     mark.move();
+     mark.turnLeft();
+     mark.move();
+     mark.turnAround(); 
      }
 }
